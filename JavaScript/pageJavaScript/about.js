34f653f37dba2623cfd363de8e209b99bf7feffd
Ryan Wahl <rwahl005@west-mec.org>
2019-04-08 21:19:35 -0700

    Finished Dropdown
    Finished the dropdown menus for the about page. (The css and javascript for them, not the content).

--- JavaScript/pageJavaScript/about.js
+++ JavaScript/pageJavaScript/about.js
@@ -1,23 +1,44 @@
 window.addEventListener("load", aboutLoad);
 
 var locActive = false;
-
+var curActive = false;
 function aboutLoad() {
     var location = document.getElementById("location");
-    location.getElementsByTagName("div")[0].style.display = "none";
-    location.onclick = locClick;
+    // location.getElementsByTagName("div")[0].style.display = "none";
+    location.getElementsByTagName("h2")[0].onclick = locClick;
+    var cur = document.getElementById("cur");
+    // location.getElementsByTagName("div")[0].style.display = "none";
+    cur.getElementsByTagName("h2")[0].onclick = curClick;
 }
 
 function locClick() {
     if (locActive) {
         var location = document.getElementById("location");
-        location.getElementsByTagName("div")[0].style.display = "none";
+        // location.getElementsByTagName("div")[0].style.display = "none";
+        location.getElementsByTagName("div")[0].className = "";
         location.getElementsByTagName("p")[0].innerHTML = "&#x22c1;";
         locActive = false;
     } else {
         var location = document.getElementById("location");
-        location.getElementsByTagName("div")[0].style.display = "block";
+        // location.getElementsByTagName("div")[0].style.display = "block";
         location.getElementsByTagName("p")[0].innerHTML = "&#x22c0;";
+        location.getElementsByTagName("div")[0].className = "open";
         locActive = true;
+    }
+}
+
+function curClick() {
+    if (curActive) {
+        var cur = document.getElementById("cur");
+        // location.getElementsByTagName("div")[0].style.display = "none";
+        cur.getElementsByTagName("div")[0].className = "";
+        cur.getElementsByTagName("p")[0].innerHTML = "&#x22c1;";
+        curActive = false;
+    } else {
+        var cur = document.getElementById("cur");
+        // location.getElementsByTagName("div")[0].style.display = "block";
+        cur.getElementsByTagName("p")[0].innerHTML = "&#x22c0;";
+        cur.getElementsByTagName("div")[0].className = "open";
+        curActive = true;
     }
 }
